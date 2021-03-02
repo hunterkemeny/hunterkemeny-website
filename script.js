51c11c916f6212ff69b00cd6f943ffe00a9f05f2
Alek Kemeny <alekkemeny@gmail.com>
2021-03-02 12:41:28 -0700

    Format with Prettier

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-let home = document.getElementById('home');
-let blog = document.getElementById('blog');
-let philosophy = document.getElementById('philosophy');
+let home = document.getElementById("home");
+let blog = document.getElementById("blog");
+let philosophy = document.getElementById("philosophy");
 
-home.onmouseover = function() {
-    home.style.textDecoration = 'underline';
-}
+home.onmouseover = function () {
+  home.style.textDecoration = "underline";
+};
 
-blog.onmouseover = function() {
-    blog.style.textDecoration = 'underline';
-}
+blog.onmouseover = function () {
+  blog.style.textDecoration = "underline";
+};
 
-philosophy.onmouseover = function() {
-    philosophy.style.textDecoration = 'underline';
-}
+philosophy.onmouseover = function () {
+  philosophy.style.textDecoration = "underline";
+};
 
-home.onmouseleave = function() {
-    home.style.textDecoration = '';
-}
+home.onmouseleave = function () {
+  home.style.textDecoration = "";
+};
 
-blog.onmouseleave = function() {
-    blog.style.textDecoration = '';
-}
+blog.onmouseleave = function () {
+  blog.style.textDecoration = "";
+};
 
-philosophy.onmouseleave = function() {
-    philosophy.style.textDecoration = '';
-}
-
+philosophy.onmouseleave = function () {
+  philosophy.style.textDecoration = "";
+};
